e88e4a3b9e752073a104c86d04f33eaeb1c50ad2
shayff <39059628+shayff@users.noreply.github.com>
2020-04-12 15:06:28 +0300

    added folder structure

--- server/hello.py
+++ server/hello.py
@@ -1,1 +1,10 @@
-print("hello")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+#for dubg
+if __name__== '__main__':
+    app.run(debug=True, port=5000)
