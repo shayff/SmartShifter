e8e6e774c534fce101b76e8538749aad93b46e42
Nely Nehemia <Nelynehemia@users.noreply.github.com>
2020-04-22 12:06:38 +0300

    Register.py - # update counter Users               # update time created

--- server/MembersService/Register.py
+++ server/MembersService/Register.py
@@ -1,10 +1,14 @@
 from pymongo import MongoClient
 from .schemas.register import validate_register
 from flask import jsonify
+from pymongo import ReturnDocument
+from datetime import datetime
 
 cluster = MongoClient("mongodb+srv://test:tester123@cluster0-pnljo.mongodb.net/test?retryWrites=true&w=majority")
 db = cluster["shifter_db"]
 collection = db["users"]
+counter = db["counters"]
+
 
 def doRegister(data):
    data = validate_register(data)
@@ -15,15 +19,18 @@ def doRegister(data):
       if result:
          return jsonify({'ok': False, 'msg': 'User with email address already exists'}), 401
       else:
-         count_doc = collection.count_documents({})
-         y = {"_id": count_doc + 1}
-         data.update(y)
+         # update counter Users
+         doc = counter.find_one_and_update({}, {"$inc": {"value": 1}}, return_document=ReturnDocument.AFTER)
+         countId = doc['value']
+         data.update({"_id": countId})
+
+         # update time created
+         date = datetime.now()
+         data.update({"time_created": date.ctime()})
+
+         # insert to db
          collection.insert_one(data)
          return jsonify({'ok': True, 'msg': 'user registered successfully'}), 200
 
    else:
-      return jsonify({'ok': False, 'msg': 'Bad request parameters: {}'.format(data['message'])}), 400
-
-
-
-
+      return jsonify({'ok': False, 'msg': 'Bad request parameters: {}'.format(data['msg'])}), 400
