20c14dff83b9e0aa3c8554c9bef93fc18213b34e
shayff <shayff@users.noreply.github.com>
2020-04-23 22:44:02 +0300

    Create companies service and added create function

---
+++ server/CompaniesService.py
@@ -0,0 +1,50 @@
+#from config import FlaskConfig
+from flask import Flask, request, jsonify
+import datetime
+import json
+from flask_jwt_extended import JWTManager, jwt_required,get_jwt_identity,get_raw_jwt
+from bson.objectid import ObjectId
+from CompaniesService.Create import doCreate
+
+class JSONEncoder(json.JSONEncoder):
+    ''' extend json-encoder class'''
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, set):
+            return list(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'JustDemonstrating'
+app.config['JWT_SECRET_KEY'] = "1asdasd#$$!1ddX"
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+jwt = JWTManager(app)
+app.json_encoder = JSONEncoder
+
+blacklist = set()
+
+@jwt.unauthorized_loader
+def unauthorized_response(callback):
+    return jsonify({
+        'ok': False,
+        'message': 'Missing Authorization Header'
+    }), 401
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return jti in blacklist
+
+@app.route("/create", methods=['POST'])
+@jwt_required
+def Login():
+    return doCreate(request.get_json())
+
+#for dubg
+if __name__== '__main__':
+    app.run(debug=True, port=5001)
---
+++ server/CompaniesService/schemas/create.py
@@ -0,0 +1,25 @@
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+from jsonschema.exceptions import SchemaError
+
+create_schema = {
+    "type": "object",
+    "properties": {
+        "company name": {
+            "type": "string"
+        }
+    },
+    "required": [
+        "company name"
+    ],
+    "additionalProperties": False
+}
+
+def validate_create(data):
+    try:
+        validate(data, create_schema)
+    except ValidationError as e:
+        return {'ok': False, 'msg': e}
+    except SchemaError as e:
+        return {'ok': False, 'msg': e}
+    return {'ok': True, 'data': data}
