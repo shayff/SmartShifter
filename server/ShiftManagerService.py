878ac85265733d75b6ce00305142faaa7a1c0bc4
Nely Nehemia <Nelynehemia@users.noreply.github.com>
2020-04-26 11:49:30 +0300

    Preparing the functions of-  -get list of shifts  -get list of employees

--- server/ShiftManagerService.py
+++ server/ShiftManagerService.py
@@ -1,9 +1,25 @@
-
+from pymongo import MongoClient
+from server.config import MongoConfig
 
 company_id = 2
+
+
+#connect to database
+cluster = MongoClient(MongoConfig['ConnectionString'])
+db = cluster[MongoConfig['ClusterName']]
+companies_collection = db["companies"]
+
 # get list of shifts
+def get_list_of_shifts():
+    company =companies_collection.find_one({'_id': company_id})
+    list_of_shifts = company['shifts']
+    return list_of_shifts
 
 # get list of employees
+def get_list_of_employees():
+    company =companies_collection.find_one({'_id': company_id})
+    list_of_employees = company['employees']
+    return list_of_employees
 
 # fot each shift, add the employee that "available" or "prefer"
 
