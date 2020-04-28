dab89aed6c9e6da0cdf69e56f2f45bdf6aed1949
Nely Nehemia <Nelynehemia@users.noreply.github.com>
2020-04-28 16:43:43 +0300

    " " ---> ' '

--- server/ShiftManagerService.py
+++ server/ShiftManagerService.py
@@ -10,7 +10,9 @@ company_id = 2
 #connect to database
 cluster = MongoClient(MongoConfig['ConnectionString'])
 db = cluster[MongoConfig['ClusterName']]
-companies_collection = db["companies"]
+companies_collection = db['companies']
+
+
 
 # get list of shifts
 def get_list_of_shifts():
@@ -21,12 +23,12 @@ def get_list_of_shifts():
 def getPartOfDayToTime():
     company = companies_collection.find_one({'_id': company_id})
     PartOfDayToTimeDict = dict()
-    for daypart in company["day parts"]:
-        PartOfDayToTimeDict[daypart["id"]] = {"start time": datetime.strptime(daypart["start time"], "%H:%M"), "end time": datetime.strptime(daypart["end time"])}
+    for daypart in company['day parts']:
+        PartOfDayToTimeDict[daypart['id']] = {'start time': datetime.strptime(daypart['start time'], '%H:%M'), 'end time': datetime.strptime(daypart['end time'])}
     return PartOfDayToTimeDict
 
 # get list of employees
-def get_list_of_employees(company_id):
+def get_list_of_employees():
     company =companies_collection.find_one({'_id': company_id})
     list_of_employees = company['employees']
     return list_of_employees
@@ -34,73 +36,73 @@ def get_list_of_employees(company_id):
 def getThePrefenceByDay(employeePrefence, day):
     #return the employee prefence of given day
     for x in employeePrefence:
-        if(x["day"] == day):
+        if(x['day'] == day):
             return x
     return None
 
 def isEmployeeCanWork(shift,employee):
-    prefnceForTheShiftDay = getThePrefenceByDay(employee["prefence"], shift["day"])
-    return shift["day part"] in prefnceForTheShiftDay["prefer"] or shift["day part"] in prefnceForTheShiftDay["available"]
+    prefnceForTheShiftDay = getThePrefenceByDay(employee['prefence'], shift['day'])
+    return shift['day part'] in prefnceForTheShiftDay['prefer'] or shift['day part'] in prefnceForTheShiftDay['available']
 
 def rankShift(shift,dictOfEmployees):
-    for employee in shift["employees_can_work"]:
-        rank=0
-        for employePrefenceOfDay in dictOfEmployees[employee["id"]]["prefence"]:
+    for employee in shift['employees_can_work']:
+        rank = 0
+        for employePrefenceOfDay in dictOfEmployees[employee['id']]['prefence']:
 
             #check if employee prefer or available to the shift
-            if employePrefenceOfDay["day"] == shift["day"]:
-                if shift["day part"] in employePrefenceOfDay["prefer"]:
+            if employePrefenceOfDay['day'] == shift['day']:
+                if shift['day part'] in employePrefenceOfDay['prefer']:
                     rank += 3
-                elif shift["day part"] in employePrefenceOfDay["available"]:
+                elif shift['day part'] in employePrefenceOfDay['available']:
                     rank += 1
         #add the employee rank given by manager
-        rank+=dictOfEmployees[employee["id"]]["rank"]
+        rank+=dictOfEmployees[employee['id']]['rank']
 
         #add the
-        rank += dictOfEmployees[employee["id"]]["count of given prefence"]-dictOfEmployees[employee["id"]]["count of shift scheduled"]
+        rank += dictOfEmployees[employee['id']]['count of given prefence']-dictOfEmployees[employee['id']]['count of shift scheduled']
 
-        employee["rank"] = rank
+        employee['rank'] = rank
     return
 
 def dictFromListOfEmployees(list_of_employees):
     result = dict()
     for employee in list_of_employees:
-        result[employee["id"]] = employee
+        result[employee['id']] = employee
     return result
 
 def updateShiftCount(dictOfEmployees):
     for key in dictOfEmployees:
         preferCount = 0
         availableCount = 0
-        for prefenceOfDay in dictOfEmployees[key]["prefence"]:
-            preferCount += len(prefenceOfDay["prefer"])
-            availableCount += len(prefenceOfDay["available"])
-        dictOfEmployees[key]["count of given prefence"] = preferCount + availableCount
-        dictOfEmployees[key]["count of shift scheduled"] = 0
+        for prefenceOfDay in dictOfEmployees[key]['prefence']:
+            preferCount += len(prefenceOfDay['prefer'])
+            availableCount += len(prefenceOfDay['available'])
+        dictOfEmployees[key]['count of given prefence'] = preferCount + availableCount
+        dictOfEmployees[key]['count of shift scheduled'] = 0
     return
 
 def getEmployeeByHigestRank(listOfShifts):
     maxRank = float('-inf')
     maxRankEmployee = None
     for shift in listOfShifts:
-        for employee in shift["employees_can_work"]:
-            if employee["rank"] > maxRank:
-                maxRank = employee["rank"]
+        for employee in shift['employees_can_work']:
+            if employee['rank'] > maxRank:
+                maxRank = employee['rank']
                 maxRankEmployee = employee
                 maxShift = shift
     return maxShift, maxRankEmployee
 
 def scheduleAllEmployeesToShift(shift, dictOfEmployees, listOfShifts):
-    while(shift["employees_can_work"]):
-        scheduleEmployeeToShift(shift["employees_can_work"][0], shift, dictOfEmployees, listOfShifts)
+    while(shift['employees_can_work']):
+        scheduleEmployeeToShift(shift['employees_can_work'][0], shift, dictOfEmployees, listOfShifts)
     return
 
 def scheduleEmployeeToShift(employee, shiftToSchedule, dictOfEmployees, listOfShifts):
-    dictOfEmployees[employee["id"]]["count of shift scheduled"] += 1
+    dictOfEmployees[employee['id']]['count of shift scheduled'] += 1
     for shift in listOfShifts:
-        if shift["day"] == shiftToSchedule["day"]:
-            shift["employees_can_work"].remove(employee)
-    shiftToSchedule["employees"].append(employee["id"])
+        if shift['day'] == shiftToSchedule['day']:
+            shift['employees_can_work'].remove(employee)
+    shiftToSchedule['employees'].append(employee['id'])
     return
 
 def buildShifts():
@@ -114,10 +116,10 @@ def buildShifts():
     updateShiftCount(dictOfEmployees)
 
     for shift in listOfShifts:
-        shift["employees_can_work"] = []
+        shift['employees_can_work'] = []
         for employee in list_of_employees:
             if isEmployeeCanWork(shift, employee):
-                shift["employees_can_work"].append({"id": employee["id"], "rank": 0})
+                shift['employees_can_work'].append({'id': employee['id'], 'rank': 0})
 
     # while there are shifts with "available" or "prefer" employee
     while listOfShifts:
@@ -127,10 +129,10 @@ def buildShifts():
         while isChanged:
             isChanged = False
             for shift in listOfShifts:
-                if shift["amount"] >= len(shift["employees_can_work"]):
+                if shift['amount'] >= len(shift['employees_can_work']):
                     scheduleAllEmployeesToShift(shift, dictOfEmployees, listOfShifts)
                     # pass shift to completed
-                    del shift["employees_can_work"]
+                    del shift['employees_can_work']
                     listOfCompleteShift.append(shift)
                     listOfShifts.remove(shift)
                     isChanged = True
@@ -148,15 +150,15 @@ def buildShifts():
         shiftToSched, employeeToSched = getEmployeeByHigestRank(listOfShifts)
         scheduleEmployeeToShift(employeeToSched, shiftToSched, dictOfEmployees, listOfShifts)
 
-        if shiftToSched["amount"] >= len(shiftToSched["employees_can_work"]) or shiftToSched["amount"] == len(shiftToSched["employees"]):
-            print("test")
+        if shiftToSched['amount'] >= len(shiftToSched['employees_can_work']) or shiftToSched['amount'] == len(shiftToSched['employees']):
+            print('test')
             # pass shift to completed
-            del shift["employees_can_work"]
+            del shift['employees_can_work']
             listOfCompleteShift.append(shift)
             listOfShifts.remove(shift)
-    print("shift that did'nt completed:")
+    print('shift that didnt completed:')
     print(listOfShifts)
-    print("list of finish shifts:")
+    print('list of finish shifts:')
     print(listOfCompleteShift)
     return
 
