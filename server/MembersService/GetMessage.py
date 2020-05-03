db2bca4ff9274b86a4e7a00387194b6b6cce315d
Nely Nehemia <Nelynehemia@users.noreply.github.com>
2020-05-03 15:28:31 +0300

    doGetMessages ready

--- server/MembersService/GetMessage.py
+++ server/MembersService/GetMessage.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from flask_jwt_extended import get_jwt_identity
 from pymongo import collection, MongoClient, ReturnDocument
 from server.config import MongoConfig
+from bson.json_util import dumps, loads
 
 # connect to database
 cluster = MongoClient(MongoConfig['ConnectionString'])
@@ -15,16 +16,13 @@ def doGetMessages():
     current_user = get_jwt_identity()
     user_in_db = users_collection.find_one({'_id': current_user['_id']})
     array_id_msg = user_in_db['messages']
-    list_of_messages = []
 
     #Search for id_messages in all messages
-    for id_msg in array_id_msg:
-        msg = messages_collection.find_one({'_id': id_msg})
-        if msg:
-            list_of_messages.append(msg)
+    msg = messages_collection.find({'_id': {'$in': array_id_msg}})
+    msg = loads(dumps(msg))
 
     #check if List is empty
-    if not list_of_messages:
-        return jsonify({'ok': True, 'msg': 'The messages list is empty'}), 401
+    if not msg:
+        return jsonify({'ok': True, 'msg': 'The messages list is empty'}), 200
     else:
-        return jsonify({'ok': True, 'msg': 'list of messages:', 'list of messages:': list_of_messages}), 401
+        return jsonify({'ok': True, 'msg': 'list of messages:', 'data': msg}), 200
