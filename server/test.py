1f07a139815260f9f05475e0bfd73ee97bb6160e
Nely Nehemia <Nelynehemia@users.noreply.github.com>
2020-08-05 16:10:23 +0300

    Test available prefer

--- server/test.py
+++ server/test.py
@@ -1,20 +1,16 @@
 import random
-available = [[]]
-prefer = [[]]
-test = [1,2,3]
-print(test.pop())
+available = [[],[],[],[],[],[],[]]
+prefer = [[],[],[],[],[],[],[]]
 
-
-for day in range(1):
+for day in range(7):
     array = [0, 1, 2]
     random.shuffle(array)
-    print(array)
-    for x in range(2):
-        if (random.randint(1, 10) > 5):
-            available[0].append(array.pop())
-    for x in range(len(array)):
-        if (random.randint(1, 10) > 7):
-            prefer[0].append(array.pop())
+    for _ in range(3):
+        if (random.randint(1, 10) > 4):
+            available[day].append(array.pop())
+    for _ in range(len(array)):
+        if (random.randint(1, 10) > 4):
+            prefer[day].append(array.pop())
 
 print(available)
 print(prefer)
