4610de066c4a0d699dd1b70ec3a04b97e2c5206d
shayff <shayff@users.noreply.github.com>
2020-08-11 23:44:43 +0300

    add sort to getshift and fix time problem in full test

--- server/ShiftManagerService/BuildShift.py
+++ server/ShiftManagerService/BuildShift.py
@@ -6,6 +6,7 @@ from .schemas.buildshift import validate_buildShift
 import pandas as pd
 import numpy as np
 from datetime import datetime
+from .BL.ShiftsLogic import sort_shifts_by_start_time
 
 MongoConfig ={
     "ConnectionString": "mongodb+srv://test:tester123@cluster0-pnljo.mongodb.net/test?retryWrites=true&w=majority",
@@ -66,36 +67,29 @@ def doBuildShift(userInput):
 
                     shift = next(x for x in list_of_shifts if x['id'] == shift_id)
 
-                    #check if shift inside the date range
-                    if shift['date'] >= data['start_date'] and shift['date'] <= data['end_date']:
-                        # For each employee id we get frmo DB the name and appened to the employees array of the shift
-                        employee_full_details_array = []
-                        for id_employee in employees_id:
-                            employee_db = users_collection.find_one({'_id': id_employee}, {'first name', 'last name'})
-                            employee_full_details_array.append(employee_db)
-                        shift['employees'] = employee_full_details_array
+                    # For each employee id we get from DB the name and appened to the employees array of the shift
+                    employee_full_details_array = []
+                    for id_employee in employees_id:
+                        employee_db = users_collection.find_one({'_id': id_employee}, {'first name', 'last name'})
+                        employee_full_details_array.append(employee_db)
+                    shift['employees'] = employee_full_details_array
 
-                        #Is_shift_full
-                        if shift['amount'] ==  len(shift['employees']):
-                            shift['Is_shift_full'] = 'full'
-                        else:
-                            shift['Is_shift_full'] = 'not_full'
-
-
-                        #add the empty shifts
-                        if shift['date'] in shift_Scheduled_to_display:
-                            shift_Scheduled_to_display[shift['date']].append(shift)
-                        else:
-                            shift_Scheduled_to_display[shift['date']] = [shift]
-
+                    #Is_shift_full
+                    if shift['amount'] ==  len(shift['employees']):
+                        shift['Is_shift_full'] = 'full'
                     else:
-                        print("check if need to delete that if")
+                        shift['Is_shift_full'] = 'not_full'
+
+                    #add the empty shifts
+                    if shift['date'] in shift_Scheduled_to_display:
+                        shift_Scheduled_to_display[shift['date']].append(shift)
+                    else:
+                        shift_Scheduled_to_display[shift['date']] = [shift]
+
+
         #sort the shifts
-        print(shift_Scheduled_to_display)
         sort_shifts_by_start_time(shift_Scheduled_to_display)
 
-        print(count)
-        print(total)
         #compute the success rate
         success_rate = int(count/total  * 100)
         return jsonify({'ok': True, 'msg': 'build shift',"success_rate": success_rate, 'data': scheduled_shifts,'Full_data': shift_Scheduled_to_display}), 200
@@ -123,9 +117,3 @@ def get_Total_Employees_Count_Needed(list_of_shifts, dates):
     for shift in list_of_shifts:
         total += shift["amount"]
     return total
-
-
-def sort_shifts_by_start_time(shifts):
-    for shifts_by_day in shifts:
-        shifts[shifts_by_day].sort(key=lambda x: x["start time"], reverse=False)
-    return
