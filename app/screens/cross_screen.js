025074aa4576906fc197aebb7b72a5a5156e6761
Nely Nehemia <Nelynehemia@users.noreply.github.com>
2020-08-18 20:45:52 +0300

    menu

--- app/screens/cross_screen.js
+++ app/screens/cross_screen.js
@@ -1,6 +1,7 @@
 import React, {useState, Component} from 'react';
-import { AsyncStorage,StyleSheet, Text, View, TextInput, Button, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { AsyncStorage,StyleSheet, Text, View } from 'react-native';
 import SubjectButton from '../component/crossScreen/sbjectSquare';
+
 
 export default class Cross_screen extends Component {
     constructor(inside){
@@ -26,31 +27,40 @@ export default class Cross_screen extends Component {
         return(
         <View style={Styles.container}>
 
-        <Text style={Styles.titelName}>{this.state.Name}</Text>
+          <View >
+            <Text style={Styles.text} >{this.state.Name}</Text>
+          </View>
 
-          <View style={Styles.line}>
+          <View style={Styles.firstLine}>
                <View>
-                    <SubjectButton fatherProps={this.props} titelName= {'User preferences'}  nevTo = {'User_preferences'}/>
-                </View>
-                <View  style={Styles.secendSubject}>
-                    <SubjectButton fatherProps={this.props} titelName= {'Shift arrangement'}  nevTo = {'Weekly_shift_arrangement'}/>
-                </View>
+                   <SubjectButton fatherProps={this.props} Icon="person-outline" titelName= { 'Profile'}  nevTo = {'Private_profile'}/>   
+               </View>
           </View>
 
-          <View style={Styles.line}>
-                <View>
-                    <SubjectButton fatherProps={this.props} titelName= {'Switching shifts'}  nevTo = {'Switching_shifts'}/>
+                <View  style={Styles.line}>
+                  <View>
+                    <SubjectButton fatherProps={this.props} Icon="event-available" titelName= {' Shift arrangement'}  nevTo = {'Weekly_shift_arrangement'}/>
+                  </View>
                 </View>
-                <View style={Styles.secendSubject}>
-                   <SubjectButton fatherProps={this.props} titelName= {'Messages'}  nevTo = {'Messages'}/>
+          
+                <View  style={Styles.line}>
+                  <View>
+                    <SubjectButton fatherProps={this.props} Icon="swap-horiz" titelName= {' Switching shifts'}  nevTo = {'Switching_shifts'}/>
+                    </View>
                 </View>
-          </View>
 
-          <View style={Styles.line}>
-                <View>
-                    <SubjectButton fatherProps={this.props} titelName= {'Private Profile'}  nevTo = {'Private_profile'}/>
+                <View  style={Styles.line}>
+                  <View>
+                   <SubjectButton fatherProps={this.props} Icon="message" titelName= {' Messages'}  nevTo = {'Messages'}/>
+                   </View>
                 </View>
-          </View>
+          
+
+                <View  style={Styles.line}>
+                  <View>
+                <SubjectButton fatherProps={this.props} Icon="today"  titelName= {' User preferences'}  nevTo = {'User_preferences'} />
+                </View>
+               </View>
 
         </View>
       );
@@ -60,28 +70,29 @@ export default class Cross_screen extends Component {
 }
 
 const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#36485f",// צבע רקע של כל האפליקציה
+    alignItems: 'center'
+  },
   line:
   {
     flexDirection : 'row',
     alignItems: 'stretch',
-    paddingLeft: 25,
-    paddingTop: 10,
-    backgroundColor:'#36485f',
+    paddingTop: 5,
+    backgroundColor:'#36485f',// צבע רגע של השורה
   },
-  container:
-  {
-    flex:1,
-    backgroundColor:'#36485f',
+  firstLine:{
+    
+    flexDirection : 'row',
+    alignItems: 'stretch',
+    paddingTop: 50,
+    backgroundColor:'#36485f',// צבע רגע של השורה
   },
-  secendSubject: {
-    paddingLeft:30,
-
-
-  },
-  titelName:
-  {
-    textAlign: 'center',
-    color: "#ffff",
-    fontSize:20,
+  text: {
+    fontSize: 40,
+    paddingTop: 30,
+    color:"#638cb0",
   }
+
 });
