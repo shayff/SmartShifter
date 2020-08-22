f10e3505ceb237d39b06eb4be4c2027fc59ed8db
shayff <shayff@users.noreply.github.com>
2020-08-22 12:11:06 +0300

    refactor and remove old code

--- server/MembersService/Profile.py
+++ server/MembersService/Profile.py
@@ -10,14 +10,14 @@ usersCollection = db['users']
 companies_collection = db['companies']
 
 def doProfile():
-    loggedinUser = get_jwt_identity()
+    logged_in_user = get_jwt_identity()
     #Search for user in database
-    user = usersCollection.find_one({'_id': loggedinUser['_id']})
+    user = usersCollection.find_one({'_id': logged_in_user['_id']})
     if(user['company']):
         company_id = user['company']
         company = companies_collection.find_one({'_id': company_id})
         user['company name']= company['company name']
 
-    print(user)
     del user['password']
+    print(user)
     return jsonify({'ok': True, 'data': user}), 200
