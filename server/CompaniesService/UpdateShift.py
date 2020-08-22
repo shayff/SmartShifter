3d4fd3a96f48982c10b515905c659d7abd52a256
shayff <shayff@users.noreply.github.com>
2020-08-22 12:09:36 +0300

    refactor updateshift

--- server/CompaniesService/UpdateShift.py
+++ server/CompaniesService/UpdateShift.py
@@ -10,30 +10,28 @@ db = cluster[MongoConfig['ClusterName']]
 companies_collection = db["companies"]
 users_collection = db["users"]
 
-def doUpdateShift(data):
-    data = validate_updateshift(data)
+def doUpdateShift(user_input):
+    data = validate_updateshift(user_input)
     if data["ok"]:
-        data = data["data"]
+        shift_to_update = data["data"]
 
         #check if user has company
-        current_user = get_jwt_identity()
-        result = users_collection.find_one({'_id': current_user['_id']})
+        logged_in_user = get_jwt_identity()
+        result = users_collection.find_one({'_id': logged_in_user['_id']})
 
         if "company" in result:
             #update data of relevant company
             company_id = result["company"]
-            print(company_id)
-            shift_id = data['id']
-            print(shift_id)
+            shift_id = shift_to_update['id']
+
             shift = companies_collection.find_one({'_id': company_id},
                                                 {"shifts": {"$elemMatch": {"id": shift_id}}})
-            print(shift)
             shift = shift["shifts"][0]
 
             #Update only the field that we need
             for key, value in shift.items():
-                if key in data:
-                    shift[key] = data[key]
+                if key in shift_to_update:
+                    shift[key] = shift_to_update[key]
 
             #update in database
             doc = companies_collection.update_one({'_id': company_id, 'shifts.id': shift_id}, {'$set':
