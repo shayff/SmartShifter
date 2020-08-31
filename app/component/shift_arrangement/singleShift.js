27e69cdd133c3f726ac9e1746955a4ad0311e3c3
Nely Nehemia <Nelynehemia@users.noreply.github.com>
2020-08-31 10:14:42 +0300

    SingleShift ->class

--- app/component/shift_arrangement/singleShift.js
+++ app/component/shift_arrangement/singleShift.js
@@ -1,48 +1,87 @@
 import React, {useState, Component} from 'react';
-import { StyleSheet, Text, View,  TouchableOpacity } from 'react-native';
+import { AsyncStorage,StyleSheet, Text, View,  TouchableOpacity, Alert} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import meneger_server from '../../networking/shiftManager_server';
 
 
-export default function SingleShift(promp) {
-
+export default class SingleShift extends Component {
     
-    const [employees, SetEmployees] = useState(promp.item.employees);
+    constructor(inside){
+        super(inside);
+        this.state = {
+            employees : this.props.item.employees,
+            _id : "",
+            isSendSwap: false,
+        }                      
+      }
 
-    let myId=12;
-    let ButtonStateHolder=true;
-      
+      componentDidMount = async () => {
+    
+        let my_id = await AsyncStorage.getItem('_id');
+        this.setState({_id:my_id});
+    }
 
+    want_to_swap = async () =>
+    {
+        let toSend = {
+            "shift_id": Number(this.props.item.id)
+        }
+        let token = await AsyncStorage.getItem('token');
+
+        const response = await meneger_server.post('/AskShiftSwap',
+        toSend,
+         {
+              headers: {
+                  Authorization: "Bearer " + token
+              }
+          }).then(response => {
+
+            this.setState({isSendSwap:true});
+            return  response.data;
+          }).catch(err => {
+            Alert.alert("something get wrong, please try again");
+    
+          });
+
+    }
+
+      render() { 
 return (
 
     <View style={Styles.itemSet}>
         <View style={Styles.titleLine}>
             <Text style={Styles.title}>Type of job: </Text>
-            <Text style={Styles.secendTitle}>{promp.item.title}</Text>
+            <Text style={Styles.secendTitle}>{this.props.item.title}</Text>
         </View>
 
         <View style={Styles.line}>
             <Text style={Styles.title}>Start time: </Text>
-            <Text style={Styles.secendTitle}>{promp.item["start time"]}</Text>
+            <Text style={Styles.secendTitle}>{this.props.item["start time"]}</Text>
         </View>
 
         <View style={Styles.line}>
             <Text style={Styles.title}>End time: </Text>
-            <Text style={Styles.secendTitle}>{promp.item["end time"]}</Text>
+            <Text style={Styles.secendTitle}>{this.props.item["end time"]}</Text>
  
         </View>
          <View>
              <Text style={Styles.title}>Employees:</Text>
                 <View>
-                    { employees.map( item => (
+                    { this.state.employees.map( item => (
                         <View key = {item._id} style={Styles.line}>
                             <Text style={Styles.nameEMP}>{item["first name"]} {item["last name"]}</Text>
-                            <View style={Styles.screenContainer} >
-                                
-                                <TouchableOpacity style={Styles.appButtonContainer} disabled = {false}  >
-                                    <MaterialIcons name="swap-horiz" size={30} color="black" />
-                                    <Text style={Styles.appButtonText}>{'swap'}</Text>
-                                </TouchableOpacity>
-                            </View>
+
+                        <View>{this.state._id == item._id ? (
+
+                                <View style={Styles.screenContainer} >
+                                    <TouchableOpacity style={this.state.isSendSwap ? Styles.appButtonContainer_disable : Styles.appButtonContainer} disabled={this.state.isSendSwap} onPress={this.want_to_swap}>
+                                        <MaterialIcons name="swap-horiz" size={30} color="black" />
+                                        <Text style={Styles.appButtonText}>{'swap'}</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            ):(null)}
+                        </View>
+
                         </View>
                         ))}
 
@@ -54,7 +93,7 @@ return (
 
 );
 
-
+}
 
 
 }
@@ -100,6 +139,16 @@ const Styles = StyleSheet.create({
         borderWidth: 2,
         borderRadius: 10,
     },
+    appButtonContainer_disable: {
+        elevation: 3,
+        backgroundColor: "#c0c0c0",
+        borderRadius: 10,
+        width:85,
+        position: 'relative', justifyContent: 'center', alignItems: 'center',
+        top: 0, left: 170, right: 10,       
+        flexDirection : 'row',
+
+      },
     appButtonContainer: {
         elevation: 3,
         backgroundColor: "#009688",
